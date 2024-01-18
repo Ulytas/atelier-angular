235c68959b977f8d107fb2743f4dbc52489d2772
Ulytas <francois.tang123@gmail.com>
2024-01-18 17:31:12 +0100

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
+//import { ProduitService } from './services/produit.service';
+import { FormsModule } from '@angular/forms';
+import { UpdateProduitComponent } from './update-produit/update-produit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProduitsComponent,
+    AddProduitComponent,
+    UpdateProduitComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+  //  ProduitService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+  styleUrl: './produits.component.css'
+})
+
+
+export class ProduitsComponent implements OnInit {
+  produits : Produit[];
+  ngOnInit(): void {}
+  constructor(private produitService: ProduitService ) {
+    this.produits = produitService.listeProduits();
+    }
+    supprimerProduit(p: Produit)
+{
+//console.log(p);
+let conf = confirm("Etes-vous sûr ?");
+if (conf)
+this.produitService.supprimerProduit(p);
+}
+}
+
+
+
