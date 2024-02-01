777fb739ab673973c2ec883045c5499cccedf47f
Ulytas <francois.tang123@gmail.com>
2024-02-01 12:30:10 +0100

    Ajouter le champ catégorie au formulaire de modification FIN Atelier 4

--- src/app/update-produit/update-produit.component.ts
+++ src/app/update-produit/update-produit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProduitService } from '../services/produit.service';
 import { Produit } from '../model/produit.model';
-
+import { Categorie } from '../model/categorie.model';
 
 @Component({
   selector: 'app-update-produit',
@@ -13,17 +13,30 @@ import { Produit } from '../model/produit.model';
 
 export class UpdateProduitComponent implements OnInit {
 currentProduit = new Produit();
+categories! : Categorie[];
+updatedCatId! : number;
 constructor(private activatedRoute: ActivatedRoute,
   private router :Router,
   private produitService: ProduitService) { }
-ngOnInit() {
+/* ngOnInit() {
 // console.log(this.route.snapshot.params.id);
 this.currentProduit = this.produitService.consulterProduit(this.activatedRoute.snapshot. params['id']);
 console.log(this.currentProduit);
-}
+} */
+ngOnInit(): void {
+  this.categories = this.produitService.listeCategories();
+  this.currentProduit =
+  this.produitService.consulterProduit(this.activatedRoute.snapshot.params['id']);
+  this.updatedCatId=this.currentProduit.categorie!.idCat;
+  //   this.updatedCatId=this.currentProduit.categorie?.idCat;
+  // .categorie?.idCat ou .categorie.idCat ne fonctionnent pas : avec "." pas possible et avec "?"
+  // le type de categorie et idCat est undefined. On attend/expect updatedCatID de type number
+  // d'être affecté à du type number. Avec "?" on ne sait pas mais avec "!" oui.
+  }
 
 updateProduit()
 { //console.log(this.currentProduit);
+this.currentProduit.categorie=this.produitService.consulterCategorie(this.updatedCatId);
 this.produitService.updateProduit(this.currentProduit);
 this.router.navigate(['produits']);
 }
